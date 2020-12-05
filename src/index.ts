2cb00e0679d43069b510fdf8f8830f389865c374
Stephan-MDD <stephan.mdd@gmail.com>
2020-12-06 00:30:06 +0100

    Quick Commit

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,25 @@ import eurekaClient from './eureka';
 import timeout from './utils/timeout';
 
 const EXPRESS_SERVER_ID: string = 'express-service';
+const FLASK_SERVER_ID: string = 'flask-service';
 
 (async () => {
 	await eurekaClient.start();
 	await timeout(500);
 
+	/** flask server (python) */
+	const flaskServer = await eurekaClient.getInstancesByAppId(FLASK_SERVER_ID)[0];
+	const FLASK_SERVICE_HOST = flaskServer.hostName;
+	const FLASK_SERVICE_PORT = flaskServer.port['$'];
+
+	console.log('FLASK SERVICE HOST::', FLASK_SERVICE_PORT);
+	console.log('FLASK SERVICE POST::', FLASK_SERVICE_HOST);
+
+	const flaskResponse = await fetch(`http://${FLASK_SERVICE_HOST}:${FLASK_SERVICE_PORT}`);
+	const flaskResponseBody = await flaskResponse.json();
+	console.log('response::', flaskResponseBody);
+
+	/** express server (typescript) */
 	const expressServer = await eurekaClient.getInstancesByAppId(EXPRESS_SERVER_ID)[0];
 	const EXPRESS_SERVICE_HOST = expressServer.hostName;
 	const EXPRESS_SERVICE_PORT = expressServer.port['$'];
@@ -15,7 +29,7 @@ const EXPRESS_SERVER_ID: string = 'express-service';
 	console.log('EXPRESS SERVICE HOST::', EXPRESS_SERVICE_HOST);
 	console.log('EXPRESS SERVICE POST::', EXPRESS_SERVICE_PORT);
 
-	const response = await fetch(`http://${EXPRESS_SERVICE_HOST}:${EXPRESS_SERVICE_PORT}`);
-	const raw = await response.json();
-	console.log('response::', raw);
+	const expressResponse = await fetch(`http://${EXPRESS_SERVICE_HOST}:${EXPRESS_SERVICE_PORT}`);
+	const expressResponseBody = await expressResponse.json();
+	console.log('response::', expressResponseBody);
 })();
